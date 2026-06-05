b02006f57e4a73fa394be173a785218a029ccd5d
pranay-prog <pranayjain2121@gmail.com>
2026-06-05 12:24:38 +0530

    Added path parameters routes

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
+#users dynamic routes 
+@app.get("/users/{user_id}")
+def get_users(user_id:int):
+    return{"user_id": user_id }
+
+@app.get("/marks/{marks_scored}")
+def get_marks(marks_scored:int):
+    return{"marks_scored": marks_scored}
+
+@app.get("/names/{name_ofstudent}")
+def get_names(name_ofstudent:str):
+    return{"name_ofstudent":name_ofstudent}
